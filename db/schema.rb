c44b572db9621b0e5a0c11193625d28271b02db1
Your <scarlet3628@likelion.org>
2021-05-10 14:02:37 +0900

    added admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_10_043629) do
+ActiveRecord::Schema.define(version: 2021_05_10_044859) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string "slug", null: false
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 2021_05_10_043629) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
