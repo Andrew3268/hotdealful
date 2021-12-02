099ed8416e602e30e503c7d98c06b7b7f0b0a49e
Your <scarlet3628@likelion.org>
2021-12-02 17:57:17 +0900

    top again

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -6,6 +6,8 @@ class HomesController < ApplicationController
   def index
     @homes = Home.all.order("created_at DESC")
     @coupons = Coupon.all.order("created_at DESC").limit(40)
+    @coupons_top = Coupon.most_hit(1.day.ago, 10)
+    @deals_top = Coupon.most_hit(1.day.ago, 10)
   end
 
   # GET /homes/1 or /homes/1.json
