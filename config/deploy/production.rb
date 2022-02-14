e1cb1d57f3babd7c918eb89c056b2d950284b837
Your <scarlet3628@likelion.org>
2022-02-14 20:44:53 +0900

    change ip address

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '157.245.128.121', user: 'deploy', roles: %w{app db web}
+server '147.182.251.41', user: 'deploy', roles: %w{app db web}
 set :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 
