7784267f3d86ecf5f8505f3e29aca788c35a9d2f
Your <scarlet3628@likelion.org>
2022-09-13 00:21:40 +0900

    share ko limited

--- app/controllers/bridges_controller.rb
+++ app/controllers/bridges_controller.rb
@@ -27,7 +27,7 @@ class BridgesController < ApplicationController
   end
 
   def share_ko_people
-    @coupons = Coupon.all.order("created_at DESC").limit(100)
+    @coupons = Coupon.all.order("created_at DESC").limit(150)
   end
 
   # def new_index
