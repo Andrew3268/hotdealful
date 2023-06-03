7b1408902dd730c38b3ef5eeea643f52d8767f92
Your <scarlet3628@likelion.org>
2023-06-03 14:54:15 +0900

    blogggg

--- app/controllers/bloggers_controller.rb
+++ app/controllers/bloggers_controller.rb
@@ -8,6 +8,8 @@ class BloggersController < ApplicationController
     @bloggers = Blogger.all.order("created_at DESC")
     @latest_blog = Blogger.last(1)
     @pagy, @except_first_blogs = pagy(Blogger.order("id desc").offset(1), items: 10)
+    @except_first_blogs = Blogger.order("id desc").offset(1).limit(10)
+    
     @picks = Pick.all.all.order("created_at DESC").limit(1)
     @most_hit = Blogger.most_hit(1.day.ago, 5)
     @coupons = Coupon.all.order("created_at DESC").limit(5)
