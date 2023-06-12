9aadad7828aaffc964e94ba04a5dc90ce7790902
Your <scarlet3628@likelion.org>
2023-06-12 14:46:14 +0900

    html

--- app/controllers/bloggers_controller.rb
+++ app/controllers/bloggers_controller.rb
@@ -27,23 +27,23 @@ class BloggersController < ApplicationController
     @hotdeals = Hotdeal.all.order("created_at DESC").limit(5)
 
      
-    set_meta_tags revierse: true,
-                  description: @blogger.bl_description,
-                  keywords: 'amazondeals, hotdeals, promotion code, coupons,',
-                  twitter: {
-                    card: "summary",
-                    site: "@thefiive",
-                    title: @blogger.bl_title,
-                    description: @blogger.bl_description,
-                    image: @blogger.bl_image
-                  },
-                  og: {
-                    title: @blogger.bl_title,
-                    description: @blogger.bl_description,
-                    type: 'website',
-                    url: blogger_url(@blogger),
-                    image: @blogger.bl_image
-                  }
+    # set_meta_tags revierse: true,
+    #               description: @blogger.bl_description,
+    #               keywords: 'amazondeals, hotdeals, promotion code, coupons,',
+    #               twitter: {
+    #                 card: "summary",
+    #                 site: "@thefiive",
+    #                 title: @blogger.bl_title,
+    #                 description: @blogger.bl_description,
+    #                 image: @blogger.bl_image
+    #               },
+    #               og: {
+    #                 title: @blogger.bl_title,
+    #                 description: @blogger.bl_description,
+    #                 type: 'website',
+    #                 url: blogger_url(@blogger),
+    #                 image: @blogger.bl_image
+    #               }
   end
 
   def hashtags
